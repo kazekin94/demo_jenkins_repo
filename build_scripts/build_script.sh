bf01a5c760d514ce721a170e59886eef2ed6f609
Ankit <ankit.thakur@juspay.in>
2020-04-15 23:35:48 +0530

    cc

--- build_scripts/build_script.sh
+++ build_scripts/build_script.sh
@@ -6,7 +6,7 @@ sudo docker build -t $imageName .
 
 sudo docker tag $imageName 614083026494.dkr.ecr.ap-south-1.amazonaws.com/demo-jenkins/$imageName
 
-$(aws ecr get-login-password --region $awsRegion )
+$(aws ecr get-login --no-include-email --region $awsRegion )
 
 sudo docker push 614083026494.dkr.ecr.ap-south-1.amazonaws.com/demo-jenkins/$imageName
 
