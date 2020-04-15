b0fcaab66f042c12b3929e3c8663a55adebaeb6f
Ankit <ankit.thakur@juspay.in>
2020-04-15 23:24:03 +0530

    gggg

--- build_scripts/build_script.sh
+++ build_scripts/build_script.sh
@@ -6,7 +6,7 @@ sudo docker build -t $imageName .
 
 sudo docker tag $imageName $awsAccountId.dkr.ecr.$awsRegion.amazonaws.com/$imageName
 
-aws ecr get-login-password --region $awsRegion | docker login --username AWS --password-stdin $awsAccountId.dkr.ecr.$awsRegion.amazonaws.com
+$(aws ecr get-login --region $awsRegion )
 
 sudo docker push $awsAccountId.dkr.ecr.$awsRegion.amazonaws.com/$imageName
 
