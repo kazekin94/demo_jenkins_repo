dcf5ac94b2236db15595a32c805da96f2840d936
Ankit <ankit.thakur@juspay.in>
2020-04-30 15:25:24 +0530

    ll

--- build_scripts/build_script.sh
+++ build_scripts/build_script.sh
@@ -4,7 +4,15 @@ cd $WORKSPACE
 
 sudo docker build -t kuzuri1194/ankit:django_helloworld .
 
-sudo docker login -u $dockerUser -p $dockerUserPasswd
+echo "Get para user name"
+ssmGetParaName=$(aws ssm get-parameter --name "docker-username" --region $awsRegion | grep Value | tr -d Value | tr -d '"' | tr -d ',' | tr -d ':' | awk '{$1=$1};1'  )
+echo $ssmGetParaName
+
+echo "Get para password"
+ssmGetParaPwd=$(aws ssm get-parameter --name "docker-password" --region $awsRegion | grep Value | tr -d Value | tr -d '"' | tr -d ',' | tr -d ':' | awk '{$1=$1};1'  )
+echo $ssmGetParaPwd
+
+sudo docker login -u $ssmGetParaName -p $ssmGetParaPwd
 
 sudo docker push kuzuri1194/ankit:django_helloworld
 
